8f19741357c6d3465a92a6dd1b58ff5b8328e63e
Gulshan Kumar <gulshansutey@gmail.com>
2022-11-13 22:56:16 +0530

    New/main screen (#3)
    * Added Navigation
    
    * Added nav

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, View } from 'react-native';
-
+import { View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
+import MainScreen from './screens/MainScreen';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
 
   return (
     <View style={{ flex: 1 }}>
       <StatusBar />
-      <LoginScreen />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home'>
+          <Stack.Screen name="login" component={LoginScreen} />
+          <Stack.Screen name="home" component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </View>
   );
 }
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,7 +1,60 @@
-import{View} from 'react-native'
+import { View, Pressable, StyleSheet, Platform, ToastAndroid, Alert, SafeAreaView, ScrollView, Text } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
+function MainScreen() {
+    function addNode() {
+        if (Platform.OS === 'android') {
+            ToastAndroid.show("Toast", ToastAndroid.SHORT)
+        } else {
+            Alert.alert("Toast")
+        }
+    }
 
-function MainScreen(){
+    return <SafeAreaView style={style.safeArea}>
 
+        <View style={style.parent}>
+
+            <View style={style.bottomBar}>
+                <Pressable
+                    style={({ pressed }) => pressed ? [style.iosPressed, style.addButton] : style.addButton}
+                    onPress={addNode}>
+                    {
+                        ({ pressed }) => (<MaterialIcons name="post-add" size={28} color={pressed ? "#e7e7e7" : "#1f55b8"} />)
+                    }
+                </Pressable>
+            </View>
+
+        </View>
+    </SafeAreaView>
 }
 
 export default MainScreen;
+
+const style = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+    parent: {
+        flex: 1,
+    },
+    bottomBar: {
+        bottom: 0,
+        width: "100%",
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        position: 'absolute',
+        borderTopColor: "#d6d6d6",
+        borderTopWidth: 1,
+    },
+    addButton: {
+        elevation: 4,
+        padding: 12,
+        shadowRadius: 4,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3
+    },
+    iosPressed: {
+        opacity: 0.9
+    },
+
+});
