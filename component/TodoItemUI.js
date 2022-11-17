0400a8d9bde48dcebbb7210e1af20233e8f68303
Gulshan-mac <gulshansutey@gmail.com>
2022-11-17 12:23:15 +0530

    Todo item UI

--- component/TodoItemUI.js
+++ component/TodoItemUI.js
@@ -4,7 +4,10 @@ function TodoItemUI({ todo, onPress }) {
     const handleItemClick = () => { onPress(todo.id) }
     console.log(todo);
     return <View>
-        <Pressable onPress={handleItemClick}>
+        <Pressable
+            style={({ pressed }) => pressed ? { backgroundColor: Colors.accentLightRipple } : {}}
+            android_ripple={{ color: Colors.accentLight }}
+            onPress={handleItemClick}>
             <View style={styles.container}>
                 <Text style={styles.text}>{todo.title}</Text>
             </View>
@@ -17,12 +20,20 @@ export default TodoItemUI;
 const styles = StyleSheet.create({
     text: {
         fontSize: 18,
-        padding:5,
+        padding: 5,
         color: Colors.textColorDark
     },
     container: {
         flex: 1,
-        marginTop: 1,
+        padding: 20,
+        margin: 5,
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        elevation: 4,
+        shadowRadius: 2,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
         alignItems: 'center',
         flexDirection: 'row'
     },
